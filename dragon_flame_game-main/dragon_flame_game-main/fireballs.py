35405267b8f1b42b506f78c7b4f47b6e9987c672
Kauã Sales <119974952+kauasales@users.noreply.github.com>
2023-02-17 17:52:09 -0400

    Update fireballs.py

--- dragon_flame_game-main/dragon_flame_game-main/fireballs.py
+++ dragon_flame_game-main/dragon_flame_game-main/fireballs.py
@@ -2,12 +2,14 @@ import pygame
 import config
 import dragon
 import game
-class Fireball(pygame.sprite.Sprite):
 
-    def __init__(self, posx, posy, spriteoffsetx, spriteoffsety, fireball_dir, enemy_pos, enemy_shield, num_bullet, bullet_desapear):
+
+class Fireball(pygame.sprite.Sprite):
+    def __init__(self, posx, posy, spriteoffsetx, spriteoffsety, fireball_dir, enemy_pos, enemy_shield, 
+                 num_bullet, bullet_desapear):
         super().__init__()
         self.image = config.fireball_img
-        self.rect = self.image.get_rect(center = (posx + spriteoffsetx, posy + spriteoffsety))
+        self.rect = self.image.get_rect(center=(posx + spriteoffsetx, posy + spriteoffsety))
         self.enemy_pos = pygame.Rect(enemy_pos)
         self.fireball_dir = fireball_dir
         self.enemy_shield = enemy_shield
@@ -26,7 +28,6 @@ class Fireball(pygame.sprite.Sprite):
 
     def update(self):
 
-
         self.rect.x += self.fireball_dir
 
         if self.rect.x > config.screen_width or self.rect.x < 0:
@@ -38,10 +39,3 @@ class Fireball(pygame.sprite.Sprite):
 
         if self.rect.colliderect(self.enemy_shield):
             self.kill()
-
-
-
-
-
-
-
