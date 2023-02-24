18f00efb70e54c23c20907e116e3c4ed4acbdce9
Kauã Sales <119974952+kauasales@users.noreply.github.com>
2023-02-24 03:42:33 -0400

    Update fireballs.py

--- dragon_flame_game-main/dragon_flame_game-main/fireballs.py
+++ dragon_flame_game-main/dragon_flame_game-main/fireballs.py
@@ -5,21 +5,18 @@ import game
 
 
 class Fireball(pygame.sprite.Sprite):
-    def __init__(self, dragon, posx, posy, spriteoffsetx, spriteoffsety, fireball_dir, enemy_pos, enemy_shield, my_shield,):
+    def __init__(self, sprite, dragon, posx, posy, spriteoffsetx, spriteoffsety, fireball_dir,
+                 enemy_pos, enemy_shield, my_shield, shield_value):
         super().__init__()
         self.size = (20, 20)
-        self.image = config.fireball_img
+        self.image = sprite
         self.rect = pygame.Rect(((posx + spriteoffsetx, posy + spriteoffsety), self.size))
         self.enemy_pos = enemy_pos.rect
         self.fireball_dir = fireball_dir
         self.enemy_shield = enemy_shield
         self.my_shield = my_shield
         self.dragon = dragon
-
-
-
-
-
+        self.shield_value = shield_value
 
     def update(self):
 
@@ -28,25 +25,9 @@ class Fireball(pygame.sprite.Sprite):
         if self.rect.x > config.screen_width + 100 or self.rect.x < 0:
             self.kill()
 
-        #elif self.rect.colliderect(self.enemy_pos):
-         #   self.kill()
-          #  self.num_bullet.add(self)
-
-        #elif self.rect.colliderect(self.enemy_shield):
-         #   self.kill()
-
-        elif self.rect.colliderect(self.my_shield):
-            self.kill()
-
+        if self.shield_value > 0:
+            if self.rect.colliderect(self.my_shield):
+                self.kill()
 
     def kill_bullet(self):
         self.kill()
-
-
-
-
-
-
-
-
-
